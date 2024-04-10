c81b391e6d3d7bbbfb0484178b85428928f5677b
jayyif <80955596+jayyif@users.noreply.github.com>
2024-04-10 19:30:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const typed = new Typed('.typed', {
+    strings: [
+        'a Cat',
+        'a professional eater',
+        'a professional sleeper',
+        'a professional groomer',
+        'annoying'
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 30,
+    backDelay: 2000
+});
